26071a0375d4963dc5724126986958e87b957fde
KONNO Kiyotaka <konnokiyotaka@gmail.com>
2018-04-29 04:22:49 +0000

    music-info更新

--- wp-content/themes/ck200/music-info.php
+++ wp-content/themes/ck200/music-info.php
@@ -35,12 +35,17 @@ global $wpdb;
 // ★デバッグ用
 $wpdb->show_errors();
 	
-$sql = $wpdb->prepare("SELECT MT.music_nm, MT.cnt_lylics CNT, COUNT( DISTINCT TL.lylics ) DISTCNT
-, CONCAT( LEFT( MT.LYLICSTR, 20 ) ,  '...' ) LYL, MS.img_url
+$sql = $wpdb->prepare("
+SELECT MT.music_nm, MT.cnt_lylics CNT, COUNT( DISTINCT TL.lylics ) DISTCNT
+/*, CONCAT( LEFT( MT.LYLICSTR, 20 ) ,  '...' ) LYL*/
+, MT.LYLICSTR LYL
+, MS.singer_nm
+, MS.img_url
 FROM lylics TL
 INNER JOIN musics MT ON MT.id = TL.music_id
 LEFT JOIN singers MS ON MT.singer_id = MS.id
-WHERE TL.music_id =%d " , $music);
+WHERE TL.music_id =%d
+" , $music);
 
 //echo $sql;
 
@@ -51,19 +56,23 @@ if($rows){
 	foreach ($rows as $row) {
 	  //$title_str =  "<p>【".$row->TITLE_NM."】</p>";
 	  $title_str =  $row->music_nm;
-	  $lylics_cnt =  $row->CNT;
-	  $lylics_distcnt =  $row->DISTCNT;
+	  $lyl_cnt =  $row->CNT;
+	  $lyl_distcnt =  $row->DISTCNT;
 	  $lyl_str =  $row->LYL;
+	  $singer_nm =  $row->singer_nm;
 	  $img_url =  isset($row->img_url) ? $row->img_url : 'https://ck200-konno.c9users.io/wp-content/uploads/2017/09/tori.jpg';
 	}
 }
 
 //マッチ情報
-$sql = $wpdb->prepare("SELECT MT.music_nm,MT.cnt_lylics CNT,COUNT( DISTINCT TL.lylics ) DISTCNT
+$sql = $wpdb->prepare("SELECT MT.id
+,MT.music_nm,MT.cnt_lylics CNT,COUNT( DISTINCT TL.lylics ) DISTCNT
 ,COUNT(DISTINCT TL1.lylics) INCNT
 ,COUNT(DISTINCT TL1.lylics)/COUNT( DISTINCT TL.lylics )
 ,CONCAT(LEFT(MT.LYLICSTR,20),'...') LYL
+,MS.singer_nm
 ,MS.img_url
+,(COUNT(DISTINCT TL1.lylics)/COUNT( DISTINCT TL.lylics )) HITRATE
  FROM lylics TL
  INNER JOIN musics MT
   ON MT.id = TL.music_id
@@ -81,13 +90,16 @@ $rows = $wpdb->get_results($sql);
 if($rows){
 	$i = 0;
 	foreach ($rows as $row) {
+	  $m_id[$i] =  $row->id;
 	  $title_strs[$i] =  $row->music_nm;
-	  $lylics_cnts[$i] =  $row->CNT;
-	  $lylics_distcnts[$i] =  $row->DISTCNT;
-	  $lylics_incnts[$i] =  $row->INCNT;
+	  $lyl_cnts[$i] =  $row->CNT;
+	  $lyl_distcnts[$i] =  $row->DISTCNT;
+	  $lyl_incnts[$i] =  $row->INCNT;
 	  $lyl_strs[$i] =  $row->LYL;
+	  $lyl_hitrate[$i] =  $row->HITRATE;
+	  $singer_nms[$i] =  $row->singer_nm;
 	  //$img_url1 =  $row->IMG_URL;
-	  $img_urls[$i] =  isset($row->img_url) ? $row->img_url : 'https://ck200-konno.c9users.io/wp-content/uploads/2017/09/tori.jpg';
+	  $img_urls[$i] =  isset($row->img_url) ? $row->img_url : '/wp-content/uploads/2017/09/tori.jpg';
 	  $i++;
 	}
 }
@@ -112,9 +124,9 @@ if($rows){
 // if($rows){
 // 	foreach ($rows as $row) {
 // 	  $title_str1 =  $row->music_nm;
-// 	  $lylics_cnt1 =  $row->CNT;
-// 	  $lylics_distcnt1 =  $row->DISTCNT;
-// 	  $lylics_incnt1 =  $row->INCNT;
+// 	  $lyl_cnt1 =  $row->CNT;
+// 	  $lyl_distcnt1 =  $row->DISTCNT;
+// 	  $lyl_incnt1 =  $row->INCNT;
 // 	  $lyl_str1 =  $row->LYL;
 // 	  //$img_url1 =  $row->IMG_URL;
 // 	  $img_url1 =  isset($row->img_url) ? $row->img_url : 'https://ck200-konno.c9users.io/wp-content/uploads/2017/09/tori.jpg';
@@ -139,9 +151,9 @@ if($rows){
 // 	if($rows){
 // 		foreach ($rows as $row) {
 // 		  $title_str2 =  $row->music_nm;
-// 		  $lylics_cnt2 =  $row->CNT;
-// 		  $lylics_distcnt2 =  $row->DISTCNT;
-// 		  $lylics_incnt2 =  $row->INCNT;
+// 		  $lyl_cnt2 =  $row->CNT;
+// 		  $lyl_distcnt2 =  $row->DISTCNT;
+// 		  $lyl_incnt2 =  $row->INCNT;
 // 		  $lyl_str2 =  $row->LYL;
 // 		  //$img_url2 =  $row->IMG_URL;
 // 		  $img_url2 =  isset($row->img_url) ? $row->IMG_URL : 'https://ck200-konno.c9users.io/wp-content/uploads/2017/09/tori.jpg';
@@ -166,9 +178,9 @@ if($rows){
 // 	if($rows){
 // 		foreach ($rows as $row) {
 // 		  $title_str3 =  $row->music_nm;
-// 		  $lylics_cnt3 =  $row->CNT;
-// 		  $lylics_distcnt3 =  $row->DISTCNT;
-// 		  $lylics_incnt3 =  $row->INCNT;
+// 		  $lyl_cnt3 =  $row->CNT;
+// 		  $lyl_distcnt3 =  $row->DISTCNT;
+// 		  $lyl_incnt3 =  $row->INCNT;
 // 		  $lyl_str3 =  $row->LYL;
 // 		  $img_url3 =  isset($row->img_url) ? $row->IMG_URL : 'https://ck200-konno.c9users.io/wp-content/uploads/2017/09/tori.jpg';
 // 		}
@@ -200,30 +212,53 @@ get_header(); ?>
 <?php } ?>
 
 <div class="page-content">
-	<img class='img-circle pull-left img-responsive' style='padding:0;margin:0 15px 15px 0;' src='<?php echo $img_url; ?>'>
-    <div class="entry-title"><?php echo $title_str; ?></div>
-	<div><?php echo $lyl_str; ?></div>
-	<div>文字数　　:<?php echo $lylics_cnt; ?></div>
-	<div>文字種類数:<?php echo $lylics_distcnt; ?></div>
-	
+	<div class="entry-title">選択した曲の情報</div>
 	<h2 class="title-border title-bg-line pink mb30">
+		<span><?php echo $title_str; ?></span>
+	</h2>
+	<div style="width:200px"><img class='img-circle pull-left img-responsive' style='padding:0;margin:0 15px 15px 0;' src='<?php echo $img_url; ?>'></div>
+	<div><strong>アーティスト名:<?php echo $singer_nm; ?></strong></div>
+	<div>文字数　　:<?php echo $lyl_cnt; ?></div>
+	<div>文字種類数:<?php echo $lyl_distcnt; ?></div>
+	<div><?php echo $lyl_str; ?></div>
+
+	<h2 class="title-border title-bg-line purple mb30">
 		<span>次に覚える曲はこれ！！</span>
 	</h2>
-<?php for($i=0;$i<=2;$i++) { ?>
+<?php for($i=0;$i<=2;$i++) {
+
+	//残り文字取得
+	$sql = $wpdb->prepare("
+	SELECT DISTINCT lylics.lylics lyl
+	FROM lylics
+	WHERE music_id = %d
+	AND lylics NOT IN (SELECT lylics FROM lylics l2 WHERE l2.music_id = %d)
+	" ,$m_id[$i] ,$music);
+	
+	$rows_lyls = $wpdb->get_results($sql);
+	$lyls_str = "";
+	foreach ($rows_lyls as $row) {
+	  $lyls_str = $lyls_str.$row->lyl;
+	}
+
+?>
 	<h3 class="title-underblock custom orange mb30">
 		<span><?php echo $i+1; ?> <?php echo $title_strs[$i]; ?></span>
 	</h3>
 	<div class="row">
 		<div class="col-sm-12">
-			<img class='img-circle pull-left img-responsive' style='padding:0;margin:0 15px 15px 0;' src='<?php echo $img_urls[$i]; ?>'>
-			<div><?php echo $lyl_strs[$i]; ?></div>
-			<div>文字数　　:<?php echo $lylics_cnts[$i]; ?></div>
-			<div>文字種類数:<?php echo $lylics_distcnts[$i]; ?></div>
-			<div>重複種類数:<?php echo $lylics_incnts[$i]; ?></div>
+			<div style="width:110px"><img class='img-circle pull-left img-responsive' style='padding:0;margin:0 15px 15px 0;' src='<?php echo $img_urls[$i]; ?>'></div>
+			<div><strong>アーティスト名:<?php echo $singer_nms[$i]; ?></strong></div>
+			<div>文字数　　:<?php echo $lyl_cnts[$i]; ?></div>
+			<div>文字種類数:<?php echo $lyl_distcnts[$i]; ?></div>
+			<div>重複種類数:<?php echo $lyl_incnts[$i]; ?></div>
+			<div>あと<?php echo $lyl_distcnts[$i] - $lyl_incnts[$i]; ?>文字</div>
+			<div>ヒット率　:<?php echo $lyl_hitrate[$i] * 100; ?>％</div>
+			<div>残り文字　:<?php echo $lyls_str ?></div>
+			<!--<div><?php echo $lyl_strs[$i]; ?></div>-->
 		</div>
 	</div>
 <?php } ?>
-
 			</main><!-- #main -->
 		</div><!-- #primary -->
 	</div><!-- #primary -->
